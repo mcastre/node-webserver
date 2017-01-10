4b0c4f6364cbe6e070bac485c46d5b0a28798bd4
mcastre <mcastre3@gmail.com>
2017-01-10 15:18:30 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+app.set('view engine', 'hbs');
+
+// MIDDLEWARE SETUP
+// ==============================
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to add to log file.');
+    }
+  });
+  next();
+});
+
+// Uncomment if site is under maintenance
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+// HBS PARTIALS
+// ==============================
+hbs.registerPartials(__dirname + '/views/partials');
+
+// HBS HELPERS
+// ==============================
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+// ROUTES
+// ==============================
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home',
+    welcomeMessage: 'Welcome!'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    error: {
+      message: 'Uh oh. Something went wrong.'
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
